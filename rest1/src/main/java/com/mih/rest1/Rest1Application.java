03d0c5cc8b27720bd7c2100337b26cd552afbc2d
mihaita.tinta <mihaita.tinta@gmail.com>
2017-11-15 10:23:09 +0200

    scale

--- rest1/src/main/java/com/mih/rest1/Rest1Application.java
+++ rest1/src/main/java/com/mih/rest1/Rest1Application.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class Rest1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Rest1Application.class, args);
 	}
 	
-    @RequestMapping("/")
+/*    @RequestMapping("/")
     public String root() {
     	return "hello-k8s";
     }
+	*/
 }
