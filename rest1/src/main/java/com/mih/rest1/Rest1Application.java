433edd606b1012c2e88dd5f976bceb2321ecd452
mihaita.tinta <mihaita.tinta@gmail.com>
2017-11-15 10:05:38 +0200

    rolling update

--- rest1/src/main/java/com/mih/rest1/Rest1Application.java
+++ rest1/src/main/java/com/mih/rest1/Rest1Application.java
@@ -2,11 +2,19 @@ package com.mih.rest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Rest1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Rest1Application.class, args);
 	}
+	
+    @RequestMapping("/")
+    public String root() {
+    	return "hello-k8s";
+    }
 }
